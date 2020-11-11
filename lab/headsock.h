c395dadfbe99e4873aca37ae33ac95620f6779e3
YuanJiayi <yuanjiayi2000@gmail.com>
2020-11-12 05:04:16 +0800

    update header and cli

--- lab/headsock.h
+++ lab/headsock.h
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/time.h>
+#include <time.h>
 
 #define NEWFILE (O_WRONLY|O_CREAT|O_TRUNC)
 #define MYTCP_PORT 4950
@@ -20,7 +21,15 @@
 #define BUFSIZE 60000
 #define PACKLEN 508
 #define HEADLEN 8
+#define ERROR_P 0.5
 
-#define MYTCP_PORT 4950
-#define MYUDP_PORT 5350
-#define DATAUNIT 500
+struct ack_so {
+	uint8_t num;
+	uint8_t len;
+};
+
+struct packet_so {
+	uint32_t num;
+	uint32_t len;
+	char data[DATALEN];
+};
